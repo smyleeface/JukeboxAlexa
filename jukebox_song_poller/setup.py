1c88d9ae90e607fc117a238826e7e396db2037ad
smyleeface <patty.ramert@gmail.com>
2017-06-23 22:31:26 -0700

    added lests.RPi

--- jukebox_song_poller/setup.py
+++ jukebox_song_poller/setup.py
@@ -7,6 +7,6 @@ setup(
     download_url='http://github.com/jukebox_song_poller',
     install_requires=['boto3>=1.4.4'],
     tests_require=['mock>=2.0.0'],
-    packages=find_packages('jukebox_song_poller', 'RPi'),
+    packages=find_packages('jukebox_song_poller', 'tests.RPi'),
     test_suite="tests.__init__"
 )
