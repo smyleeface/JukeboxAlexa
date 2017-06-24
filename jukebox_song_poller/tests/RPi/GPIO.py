bfd67618a9591868a0befae687e2bf1e4e828c02
smyleeface <patty.ramert@gmail.com>
2017-06-23 22:45:41 -0700

    changed local RPi for tests

---
+++ jukebox_song_poller/tests/RPi/GPIO.py
@@ -0,0 +1,40 @@
+class BCM(object):
+    def __init__(self):
+        pass
+
+
+class OUT(object):
+    def __init__(self):
+        pass
+
+
+class IN(object):
+    def __init__(self):
+        pass
+
+
+class HIGH(object):
+    def __init__(self):
+        pass
+
+
+class LOW(object):
+    def __init__(self):
+        pass
+
+
+def setmode(bcm):
+    pass
+
+
+def cleanup():
+    pass
+
+
+def setup(pin, type):
+    pass
+
+
+def output(pin, action):
+    pass
+
