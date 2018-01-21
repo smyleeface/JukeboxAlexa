077ce58d6bd00a7656b2b82af0cc27d1ba6aa762
Patty Ramert <patty.ramert@gmail.com>
2018-01-21 05:11:47 +0000

    added sleep

--- song_poller/song_poller_service.sh
+++ song_poller/song_poller_service.sh
@@ -26,6 +26,7 @@ song_poller_script_path=/home/pi/jukebox/song_poller/song_poller.py
 case "$1" in
   start)
     log_begin_msg "Starting Jukebox Song Poller Service"
+    sleep 60
     cd /home/pi/jukebox
     git fetch origin
     git reset --hard origin/master
