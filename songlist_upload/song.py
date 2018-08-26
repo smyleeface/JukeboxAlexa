d8bffbf17a1c83028a608706742a9476e55e596b
Patty R <patty.ramert@gmail.com>
2018-08-26 01:12:57 -0700

    removing python ports

--- songlist_upload/song.py
+++
@@ -1,7 +0,0 @@
-class Song(object):
-    def __init__(self, number, song, artist, search_title, search_artist):
-        self.number = number
-        self.song = song
-        self.artist = artist
-        self.search_title = search_title.lower()
-        self.search_artist = search_artist.lower()
--- songlist_upload/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-
-import unittest
-
-
-def songlist_upload_testsuite():
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
