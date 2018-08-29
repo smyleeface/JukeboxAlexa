bc0c39362cdafa8b1be057fd8341b08c7773b75b
Patty R <patty.ramert@gmail.com>
2018-08-28 23:11:11 -0700

    updated build files

--- cicd/10_build.sh
+++ cicd/10_build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-for directory in JukeboxAlexa/JukeboxAlexa.*/ ; do
-    echo "coverlet ${directory}"
-    /project/tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll --target dotnet --targetargs "test ${directory} --no-build" --format opencover
-done
+set -e
 
-echo "uploading coverage"
-coverallsToken=$(aws ssm get-parameter --name /general/coveralls/token  --with-decryption --query Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-/project/tools/csmacnz.Coveralls --opencover -i coverage.opencover.xml --basePath JukeboxAlexa --repoToken ${coverallsToken}
+dotnet restore JukeboxAlexa/
+dotnet build JukeboxAlexa/
---
+++ cicd/30_coverage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+for directory in JukeboxAlexa/JukeboxAlexa.*/ ; do
+    echo "coverlet ${directory}"
+    /project/tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll --target dotnet --targetargs "test ${directory} --no-build" --format opencover
+done
+
+echo "uploading coverage"
+coverallsToken=$(aws ssm get-parameter --name /general/coveralls/token  --with-decryption --query Parameter.Value | sed -e 's/^"//' -e 's/"$//')
+/project/tools/csmacnz.Coveralls --opencover -i coverage.opencover.xml --basePath JukeboxAlexa --repoToken ${coverallsToken}
