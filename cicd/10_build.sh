62a5a2c362ff0c285a515b5e4f639abe1ec13f04
Patty R <patty.ramert@gmail.com>
2018-08-28 22:22:28 -0700

    adding full path

--- cicd/10_build.sh
+++ cicd/10_build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-ls -la
-pwd
-
 for directory in ../JukeboxAlexa.*/ ; do
     echo "coverlet $directory"
-    ./tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll --target dotnet --targetargs "test ${directory} --no-build" --format opencover
+    /project/tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll --target dotnet --targetargs "test ${directory} --no-build" --format opencover
 done
 
+echo "uploading coverage"
 coverallsToken=$(aws ssm get-parameter --name /general/coveralls/token  --with-decryption --query Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-.\tools\csmacnz.Coveralls --opencover -i coverage.opencover.xml --basePath JukeboxAlexa --repoToken ${coverallsToken}
+/project/tools/csmacnz.Coveralls --opencover -i coverage.opencover.xml --basePath JukeboxAlexa --repoToken ${coverallsToken}
 
