6a7168499b927439f1f42ac5873c007f0020510f
Patty R <patty.ramert@gmail.com>
2018-08-30 08:30:49 -0700

    removing module not ready for the project

--- JukeboxAlexa/JukeboxAlexa.FindSongRequested/IDynamodbDependencyProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using JukeboxAlexa.Library.Model;
-
-namespace JukeboxAlexa.FindSongRequested {
-    public interface IDynamodbDependencyProvider {
-        Task<IEnumerable<SongModel.Song>> DynamoDbFindSongsByNumberAsync(string title);
-    }
-}
