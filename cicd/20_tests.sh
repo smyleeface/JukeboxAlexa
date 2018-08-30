94c4dfec5cbb40c80c0df496266299bb64fd1375
Patty R <patty.ramert@gmail.com>
2018-08-29 21:19:11 -0700

    cicd updates

--- cicd/20_tests.sh
+++ cicd/20_tests.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-cd JukeboxAlexa
+CODEBUILD_SRC_DIR=/project
+cd ${CODEBUILD_SRC_DIR}/JukeboxAlexa
+
+echo "***INFO: Restoring packages"
 dotnet restore
+
+echo "***INFO: Building packages"
 dotnet build
+
+echo "***INFO: Testing solution"
 dotnet test
