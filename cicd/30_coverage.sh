e1dd1831cd18d890c8d999684a8a2f8bf91ffd16
Patty R <patty.ramert@gmail.com>
2018-09-01 13:07:24 -0700

    cleaned up build and added cross region deploy

--- cicd/30_coverage.sh
+++ cicd/30_coverage.sh
@@ -2,18 +2,30 @@
 
 set -e
 
-#dotnet tool install coveralls.net --tool-path tools
-#dotnet tool install coverlet.console --tool-path tools
-
-dotnet restore JukeboxAlexa/
-dotnet build JukeboxAlexa/
-
 coverallsToken=$(aws ssm get-parameter --name /general/coveralls/token  --with-decryption --query Parameter.Value | sed -e 's/^"//' -e 's/"$//')
 
 for directory in JukeboxAlexa/JukeboxAlexa.*/ ; do
+
     echo "coverlet ${directory}"
-    tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll --output "${directory}/coverage.xml" --target dotnet --targetargs "test ./${directory} --no-build" --format opencover --exclude-by-file "**/obj/**"
+    tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll && \
+        --output "${directory}/coverage.xml" && \
+        --target dotnet && \
+        --targetargs "test ./${directory} --no-build" && \
+        --format opencover && \
+        --exclude-by-file "**/obj/**" && \
+        --exclude-by-file "**/bin/**"
+
     echo "uploading coverage"
-    tools/csmacnz.Coveralls --commitId $CODEBUILD_SOURCE_VERSION --useRelativePaths --opencover -i ${directory}/coverage.xml --repoToken ${coverallsToken}
+    tools/csmacnz.Coveralls && \
+        --commitId $GITSHA && \
+        --commitBranch $GIT_BRANCH && \
+        --commitAuthor $GIT_AUTHOR&& \
+        --commitEmail $GIT_AUTHOR_EMAIL && \
+        --commitMessage $GIT_COMMIT_MESSAGE && \
+        --jobId $CODEBUILD_BUILD_ID && \
+        --useRelativePaths && \
+        --opencover && \
+        -i ${directory}/coverage.xml && \
+        --repoToken ${coverallsToken}
 done
 
---
+++ cicd/40_deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+TIER=$1
+CROSS_ACCOUNT_ROLE_NAME="CrossAccountRole-JukeboxAlexa"
+
+if [[ "${TIER}" == 'production' ]]; then
+    ACCOUNT_ID=$(aws ssm get-parameter --name /dev/JukeboxAlexa/account/staging  --with-decryption --query Parameter.Value | sed -e 's/^"//' -e 's/"$//')
+else
+    ACCOUNT_ID=$(aws ssm get-parameter --name /dev/JukeboxAlexa/account/production  --with-decryption --query Parameter.Value | sed -e 's/^"//' -e 's/"$//')
+fi
+
+# ${TIER} credentials
+echo "***INFO: Generating ${TIER} credentials"
+credentials=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/${CROSS_ACCOUNT_ROLE_NAME}" --role-session-name ${GITSHA})
+AWS_ACCESS_KEY_ID=echo ${credentials} | jq -r '.Credentials.AccessKeyId'
+AWS_SECRET_ACCESS_KEY=echo ${credentials} | jq -r '.Credentials.SecretAccessKey'
+AWS_SESSION_TOKEN=echo ${credentials} | jq -r '.Credentials.SessionToken'
+
+echo "***INFO: Deploying to ${TIER}"
+lash deploy --tier ${TIER}
