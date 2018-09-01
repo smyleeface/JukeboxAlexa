4bba4490e8f09224523e6e518113b60f5a8630fc
Patty R <patty.ramert@gmail.com>
2018-08-31 22:59:57 -0700

    added commit id

--- cicd/30_coverage.sh
+++ cicd/30_coverage.sh
@@ -14,6 +14,6 @@ for directory in JukeboxAlexa/JukeboxAlexa.*/ ; do
     echo "coverlet ${directory}"
     /project/tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll --output "${directory}/coverage.xml" --target dotnet --targetargs "test ./${directory} --no-build" --format opencover --exclude-by-file "**/obj/**"
     echo "uploading coverage"
-    /project/tools/csmacnz.Coveralls --commitId d1485fd3ec7d1f7ac9b6aa6694ee6c4550be69af --commitBranch feature-port-lambdasharp --useRelativePaths --opencover -i ${directory}/coverage.xml --repoToken ${coverallsToken}
+    /project/tools/csmacnz.Coveralls --commitId $CODEBUILD_SOURCE_VERSION --useRelativePaths --opencover -i ${directory}/coverage.xml --repoToken ${coverallsToken}
 done
 
