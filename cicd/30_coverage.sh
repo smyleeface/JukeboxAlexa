3b8bd50da16e22ef0f3f52a474cba25917a40321
Patty R <patty.ramert@gmail.com>
2018-09-01 19:55:52 -0700

    trying commands again

--- cicd/30_coverage.sh
+++ cicd/30_coverage.sh
@@ -8,26 +8,26 @@ coverallsToken=$(aws ssm get-parameter --name /general/coveralls/token  --with-d
 
 for directory in JukeboxAlexa/JukeboxAlexa.*/ ; do
 
-    echo "coverlet ${directory}"
-    echo "tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll \
+    echo "***INFO: coverlet ${directory}"
+    tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll \
         --output '${directory}/coverage.xml' \
         --target /usr/bin/dotnet \
         --targetargs 'test ./${directory} --no-build' \
         --format opencover \
         --exclude-by-file '**/obj/**' \
-        --exclude-by-file '**/bin/**'"
+        --exclude-by-file '**/bin/**'
 
-    echo "uploading coverage"
-    echo "tools/csmacnz.Coveralls \
+    echo "***INFO: uploading coverage"
+    tools/csmacnz.Coveralls \
         --commitId ${GITSHA} \
-        --commitBranch ${GIT_BRANCH} \
-        --commitAuthor ${GIT_AUTHOR_NAME} \
-        --commitEmail ${GIT_AUTHOR_EMAIL} \
-        --commitMessage ${GIT_COMMIT_MESSAGE} \
-        --jobId ${CODEBUILD_BUILD_ID} \
+        --commitBranch "${GIT_BRANCH}" \
+        --commitAuthor "${GIT_AUTHOR_NAME}" \
+        --commitEmail "${GIT_AUTHOR_EMAIL}" \
+        --commitMessage "${GIT_COMMIT_MESSAGE}" \
+        --jobId "${CODEBUILD_BUILD_ID}" \
         --useRelativePaths \
         --opencover \
         -i ${directory}/coverage.xml \
-        --repoToken ${coverallsToken}"
+        --repoToken ${coverallsToken}
 done
 
