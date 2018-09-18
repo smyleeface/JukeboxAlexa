1babc51960bb87a65281b876567953dea4c1c229
Patty R <patty.ramert@gmail.com>
2018-09-18 05:27:47 -0700

    update path

--- buildspec/push-coverage_reports.sh
+++ buildspec/push-coverage_reports.sh
@@ -19,7 +19,7 @@ if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
         if [[ -d $BUILD_DIRECTORY ]]; then
 
             PROJECT_NAME=${BUILD_DIRECTORY##*/}
-            COVERLET_OUTPUT=${BUILD_DIRECTORY}coverage.xml
+            COVERLET_OUTPUT=${BUILD_DIRECTORY}/coverage.xml
             
             echo "***INFO: coverlet ${COVERLET_OUTPUT}"
             ${CODEBUILD_SRC_DIR}/tools/coverlet ${BUILD_DIRECTORY}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll \
@@ -32,7 +32,7 @@ if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
                 
             # upload report to s3
             
-            echo "${COVERLET_OUTPUT} s3://${S3_BUCKET}/${REPO}/${GIT_BRANCH}/${GITSHA}/${PROJECT_NAME}"
+            echo "${COVERLET_OUTPUT} s3://${S3_BUCKET}/${REPO}/${GIT_BRANCH}/${GITSHA}/${PROJECT_NAME}/coverage.xml"
     #        aws s3 cp ${COVERLET_OUTPUT} s3://${S3_BUCKET}/${REPO}/${GIT_BRANCH}/${GITSHA}/${PROJECT_NAME}
         fi
     done
