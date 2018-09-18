a993f596bbbed91453c193da5a217e9817235d70
Patty R <patty.ramert@gmail.com>
2018-09-18 05:57:22 -0700

    coveralls

--- buildspec/coveralls.sh
+++ buildspec/coveralls.sh
@@ -4,28 +4,31 @@ set -e
 
 if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
 
-    S3_BUCKET="dev-smyleegithubeventroutes-codecoveragereports-loog4breq9fw"
+    ls -la ${CODEBUILD_SRC_DIR}
 
-    source cicd/env_vars.sh
-    
-    # download files from S3
-    mkdir coverage
-    aws s3 cp --recursive s3://${S3_BUCKET}/${REPO}/${GIT_BRANCH}/${GITSHA} ./
-
-    TOKEN=$(aws ssm get-parameter --name /general/coveralls/token  --with-decryption --query  Parameter | jq -r '.Value')
-    
-    for files in ./; do
-        echo "***INFO: uploading coverage"
-        tools/csmacnz.Coveralls \
-            --commitId ${GITSHA} \
-            --commitBranch "${GIT_BRANCH}" \
-            --commitAuthor "${GIT_AUTHOR_NAME}" \
-            --commitEmail "${GIT_AUTHOR_EMAIL}" \
-            --commitMessage "${GIT_COMMIT_MESSAGE}" \
-            --jobId "${CODEBUILD_BUILD_ID}" \
-            --useRelativePaths \
-            --opencover \
-            -i ${files} \
-            --repoToken ${TOKEN}
-    done
+#    S3_BUCKET="dev-smyleegithubeventroutes-codecoveragereports-loog4breq9fw"
+#
+#    source cicd/env_vars.sh
+#    
+#    # download files from S3
+#    mkdir coverage
+#    aws s3 cp --recursive s3://${S3_BUCKET}/${REPO}/${GIT_BRANCH}/${GITSHA} ./
+#
+#    TOKEN=$(aws ssm get-parameter --name /general/coveralls/token  --with-decryption --query  Parameter | jq -r '.Value')
+#    
+#    for files in ./; do
+#        echo "***INFO: uploading coverage"
+#        echo ${files}
+#        tools/csmacnz.Coveralls \
+#            --commitId ${GITSHA} \
+#            --commitBranch "${GIT_BRANCH}" \
+#            --commitAuthor "${GIT_AUTHOR_NAME}" \
+#            --commitEmail "${GIT_AUTHOR_EMAIL}" \
+#            --commitMessage "${GIT_COMMIT_MESSAGE}" \
+#            --jobId "${CODEBUILD_BUILD_ID}" \
+#            --useRelativePaths \
+#            --opencover \
+#            -i ${files} \
+#            --repoToken ${TOKEN}
+#    done
 fi
