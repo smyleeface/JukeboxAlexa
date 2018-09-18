cefa03f8f5570c0121d0804294ce9af39934fa4f
Patty R <patty.ramert@gmail.com>
2018-09-18 05:51:25 -0700

    coveralls

--- buildspec/coveralls.sh
+++ buildspec/coveralls.sh
@@ -4,11 +4,17 @@ set -e
 
 if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
 
+    S3_BUCKET="dev-smyleegithubeventroutes-codecoveragereports-loog4breq9fw"
+
     source cicd/env_vars.sh
+    
+    # download files from S3
+    mkdir coverage
+    aws s3 cp --recursive s3://${S3_BUCKET}/${REPO}/${GIT_BRANCH}/${GITSHA} ./
 
     TOKEN=$(aws ssm get-parameter --name /general/coveralls/token  --with-decryption --query  Parameter | jq -r '.Value')
     
-    for files in ./${REPO}/${GIT_BRANCH}/${GITSHA}/${COVERLET_OUTPUT} ; do
+    for files in ./; do
         echo "***INFO: uploading coverage"
         tools/csmacnz.Coveralls \
             --commitId ${GITSHA} \
