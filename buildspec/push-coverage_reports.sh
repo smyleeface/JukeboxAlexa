293fa069f9e4360fc596cccdb5586c7a398afe2b
Patty R <patty.ramert@gmail.com>
2018-09-19 19:51:31 -0700

    adding codacy

--- buildspec/push-coverage_reports.sh
+++ buildspec/push-coverage_reports.sh
@@ -8,30 +8,44 @@ if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
     
     coverallsToken=$(aws ssm get-parameter --name /general/coveralls/token  --with-decryption --query  Parameter | jq -r '.Value')
     codeCovToken=$(aws ssm get-parameter --name /general/codecov/token  --with-decryption --query  Parameter | jq -r '.Value')
+    codacyToken=$(aws ssm get-parameter --name /general/codacy/token  --with-decryption --query  Parameter | jq -r '.Value')
 
     for directory in ./src/JukeboxAlexa/JukeboxAlexa.*/ ; do
 
-        echo "****DIRECTORY: ${directory}"
+        echo "****PROCESSING DIRECTORY: ${directory}"
 
         cd ${CODEBUILD_SRC_DIR}
         
+        # GENERATE REPORTS - opencover
         echo "***INFO: coverlet ${directory}"
         ${CODEBUILD_SRC_DIR}/tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll \
-            --output ${directory}coverage.xml \
+            --output ${directory}coverage-opencover.xml \
             --target /usr/bin/dotnet \
             --targetargs "test ${directory} --no-build" \
             --format opencover \
             --exclude-by-file "**/obj/**" \
             --exclude-by-file "**/bin/**"
-            
+        
+        # GENERATE REPORTS - lcov
         ${CODEBUILD_SRC_DIR}/tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll \
-            --output ${directory}coverage.json \
+            --output ${directory}coverage-lcov.json \
             --target /usr/bin/dotnet \
             --targetargs "test ${directory} --no-build" \
             --format lcov \
             --exclude-by-file "**/obj/**" \
-            --exclude-by-file "**/bin/**" 
+            --exclude-by-file "**/bin/**"  
 
+        # GENERATE REPORTS - cobertura
+        ${CODEBUILD_SRC_DIR}/tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll \
+            --output ${directory}coverage-cobertura.xml \
+            --target /usr/bin/dotnet \
+            --targetargs "test ${directory} --no-build" \
+            --format cobertura \
+            --exclude-by-file "**/obj/**" \
+            --exclude-by-file "**/bin/**"
+        
+        ############################
+        # UPLOAD REPORT - Coveralls
         echo "***INFO: uploading Coveralls"
         ./tools/csmacnz.Coveralls \
             --commitId ${GITSHA} \
@@ -42,16 +56,25 @@ if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
             --jobId "${CODEBUILD_BUILD_ID}" \
             --useRelativePaths \
             --opencover \
-            -i ${directory}coverage.xml \
+            -i ${directory}coverage-opencover.xml \
             --repoToken ${coverallsToken}
 
+        # UPLOAD REPORT - CodeCov
         echo "***INFO: uploading CodeCov"
         tools/codecov \
-            -f "${directory}coverage.json" \
+            -f ${directory}coverage.json \
             -t ${codeCovToken} \
             -B ${GIT_BRANCH} \
             -C ${GITSHA} \
-            -b "${CODEBUILD_BUILD_ID}"
+            -b ${directory}coverage-lcov.json
+            
+        # UPLOAD REPORT - Codacy
+        echo "***INFO: uploading Codacy"
+        python-codacy-coverage \
+            -r ${directory}coverage-cobertura.xml \
+            -t ${codacyToken} \
+            -c ${GITSHA} \
+            -d ${CODEBUILD_SRC_DIR}
     done
 fi
 
