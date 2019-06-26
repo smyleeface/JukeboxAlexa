c2fabed92975328023e74131119d44d05f24afa9
Patty R <patty.ramert@gmail.com>
2019-06-25 19:34:22 -0700

    excluding with .nuget

--- buildspec/coverage_reports.sh
+++ buildspec/coverage_reports.sh
@@ -33,7 +33,7 @@ if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
                 --exclude-by-file "**/obj/**" \
                 --exclude-by-file "**/bin/**" \
                 --exclude "[xunit*]*" \
-                --exclude "[Microsoft.NET.Test.Sdk.Program.cs]*"
+                --exclude "[.nuget]*"
     
             # GENERATE REPORTS - lcov
             /opt/tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll \
@@ -44,7 +44,7 @@ if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
                 --exclude-by-file "**/obj/**" \
                 --exclude-by-file "**/bin/**" \
                 --exclude "[xunit*]*" \
-                --exclude "[Microsoft.NET.Test.Sdk.Program.cs]*"
+                --exclude "[.nuget]*"
     
     #        # GENERATE REPORTS - cobertura
     #        ${CODEBUILD_SRC_DIR}/tools/coverlet ${directory}bin/Debug/netcoreapp2.1/xunit.runner.visualstudio.dotnetcore.testadapter.dll \
