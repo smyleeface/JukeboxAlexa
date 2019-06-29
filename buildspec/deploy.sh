624cafc63e5238579ef74e972672cd8b23865a13
Patty R <patty.ramert@gmail.com>
2019-06-29 13:43:36 -0700

    update deploy commands

--- buildspec/deploy.sh
+++ buildspec/deploy.sh
@@ -5,22 +5,17 @@ set -e
 if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
 
     source cicd/env_vars.sh
-
     cd ${CODEBUILD_SRC_DIR}/src/JukeboxAlexa
-    export LAMBDASHARP_TIER=sandbox
     
-    TIER=${LAMBDASHARP_TIER}
-    CROSS_ACCOUNT_ROLE_NAME="CrossAccountRole-JukeboxAlexa"
-    export ACCOUNT_ID=$(aws ssm get-parameter --name /dev/JukeboxAlexa/account/${TIER}  --with-decryption --query Parameter | jq -r '.Value')
-
-    echo "***INFO: Generating ${TIER} credentials"
-    credentials=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/${CROSS_ACCOUNT_ROLE_NAME}" --role-session-name ${GITSHA})
+    # NOTE(pattyr, 20190728): LAMBDASHARP_TIER is included as an environment variable when triggering CodeBuild Project
+    # NOTE(pattyr, 20190728): CROSS_ACCOUNT_ROLE_ARN is included as an environment variable in the CodeBuild Project
+    echo "***INFO: Generating ${LAMBDASHARP_TIER} credentials"
+    credentials=$(aws sts assume-role --role-arn "${CROSS_ACCOUNT_ROLE_ARN}" --role-session-name ${GITSHA})
     export AWS_ACCESS_KEY_ID=$(echo ${credentials} | jq -r '.Credentials.AccessKeyId')
     export AWS_SECRET_ACCESS_KEY=$(echo ${credentials} | jq -r '.Credentials.SecretAccessKey')
     export AWS_SESSION_TOKEN=$(echo ${credentials} | jq -r '.Credentials.SessionToken')
 
-    echo "***INFO: Deploying to ${TIER}"
+    echo "***INFO: Deploying to ${LAMBDASHARP_TIER}"
     cd ${CODEBUILD_SRC_DIR}/src/JukeboxAlexa
-    dotnet lash deploy --tier ${TIER}
-
+    dotnet lash deploy --tier ${LAMBDASHARP_TIER}
 fi
