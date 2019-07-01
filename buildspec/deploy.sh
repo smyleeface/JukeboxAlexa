2d7841cc2a1aa487c931044eae3cb1c78cc38868
Patty R <patty.ramert@gmail.com>
2019-06-30 22:06:44 -0700

    update packages; update deploy for sandbox & prod

--- buildspec/deploy.sh
+++ buildspec/deploy.sh
@@ -11,6 +11,14 @@ if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
     # NOTE(pattyr, 20190728): LAMBDASHARP_TIER is included as an environment variable when triggering CodeBuild Project
     # NOTE(pattyr, 20190728): CROSS_ACCOUNT_ROLE_ARN is included as an environment variable in the CodeBuild Project
     echo "***INFO: Generating ${LAMBDASHARP_TIER} credentials"
+    if [ "${LAMBDASHARP_TIER}" -eq "Production" ]; then
+        CROSS_ACCOUNT_ROLE_ARN=${PRODUCTION_CROSS_ACCOUNT_ROLE_ARN}
+    elif [ "${LAMBDASHARP_TIER}" -eq "Sandbox" ]; then
+        CROSS_ACCOUNT_ROLE_ARN=${SANDBOX_CROSS_ACCOUNT_ROLE_ARN}
+    else
+        echo "Environment ${LAMBDASHARP_TIER} not supported"
+        exit 0
+    fi
     credentials=$(aws sts assume-role --role-arn "${CROSS_ACCOUNT_ROLE_ARN}" --role-session-name ${GITSHA})
     export AWS_ACCESS_KEY_ID=$(echo ${credentials} | jq -r '.Credentials.AccessKeyId')
     export AWS_SECRET_ACCESS_KEY=$(echo ${credentials} | jq -r '.Credentials.SecretAccessKey')
@@ -22,3 +30,10 @@ if [[ ${CODEBUILD_BUILD_SUCCEEDING} ]]; then
 #    lash deploy AlexaSkill --tier ${LAMBDASHARP_TIER} && \
 #    lash deploy AlexaSkillProxy --tier ${LAMBDASHARP_TIER}
 fi
+
+
+
+
+
+# TODO: getting the cross account roles on both sandbox and production, so when it's deployed it knows which tier to deploy to
+# fix account resources for sandbox, so it deploys to sandbox
